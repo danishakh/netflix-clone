dae587617a0a6696f16d67fedccb89cd31a7d4fa
Don <danishakh@gmail.com>
2021-09-23 17:20:17 -0700

    added react router, created new pages and setup basic routes in app.js to pages

--- netflix-client/src/app.js
+++ netflix-client/src/app.js
@@ -1,16 +1,23 @@
 import React from "react";
-import { BrowserRouter as Router, Switch } from "react-router-dom";
-import { JumbotronContainer } from "./containers/jumbotron";
-import { FooterContainer } from "./containers/footer";
-import { FaqsContainer } from "./containers/faqs";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
+import { Home, Browse, Signin, Signup } from "./pages";
 
 export default function App() {
 	return (
 		<Router>
-			<JumbotronContainer />
-			<FaqsContainer />
-			<FooterContainer />
+			<Route exact path={ROUTES.BROWSE}>
+				<Browse />
+			</Route>
+			<Route exact path={ROUTES.SIGN_IN}>
+				<Signin />
+			</Route>
+			<Route exact path={ROUTES.SIGN_UP}>
+				<Signup />
+			</Route>
+			<Route exact path={ROUTES.HOME}>
+				<Home />
+			</Route>
 		</Router>
 	);
 }
