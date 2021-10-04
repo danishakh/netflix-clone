262c4e477dd8bcbc1afecb2f0653c9946caccded
danishakh <danishakh@gmail.com>
2021-10-04 09:46:20 -0700

    added utility to map through and return list of movies and series by category to be used in Browse page

--- netflix-client/src/pages/browse.js
+++ netflix-client/src/pages/browse.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { useContent } from '../hooks';
+import selectionFilter from "../utils/selection-filter";
 
 export default function Browse() {
 	// need the series and movies
 	const { series } = useContent('series')
-	const { movies } = useContent('films')
+	const { films } = useContent('films')
 	// console.log('series', series)
-	// console.log('movies', movies);
+	// console.log('movies', films);
 
 	// we need slides
+	const slides = selectionFilter({ series, films});
+	console.log(slides);
 
 	// pass it to the browse container
 	return <p>Browse Page!</p>;
